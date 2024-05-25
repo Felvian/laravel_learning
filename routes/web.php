5178d34e410e52a20deff06c907bb930aab489cd
felvian <felvian221@gmail.com>
2024-05-25 12:44:37 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/','App\Http\Controllers\HomeController@index')->name('home');
+Route::get('/', [\App\Http\Controllers\MainController::class,'index'])->name('main.index');
 
 Route::group(['namespace' => 'App\Http\Controllers\Post'], function () {
     Route::get('/posts', 'IndexController')->name('post.index');
